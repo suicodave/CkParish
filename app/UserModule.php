904f6d021c34320e7351fc9accb43fe36300c1c8
suicodave <suico.dave@gmail.com>
2019-11-21 18:55:33 +0800

    home initial user modules display

---
+++ app/UserModule.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class UserModule
+{
+    const MODULES = [
+        [
+            'name' => 'Users',
+            'description' => 'User administration and management.'
+        ],
+        [
+            'name' => 'Roles and Permissions',
+            'description' => 'User roles and permissions management. Regulates user access according to the organization\'s policy.'
+        ],
+
+    ];
+}
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,25 +4,32 @@
 
 <div class="container mt-5">
     <div class="row">
-        <div class="col-12">
+        <div class="col-12 mb-3">
             <h2 class="text-center">Components</h2>
         </div>
-        
+
     </div>
     <div class="row">
-            @foreach ([1,2,3,4] as $item)
-                <div class=" col-3">
-                    <div class="card card-body module-item p-3">
-                        {{$item}}
-                    </div>
+        @foreach (App\UserModule::MODULES as $module)
+        <div class="col-3 d-flex align-items-stretch">
+            <div class="card text-white bg-dark  module-item mb-4">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        {{$module['name']}}
+                    </h5>
+                    <p class="card-text">
+                        {{$module['description']}}
+                    </p>
                 </div>
-            @endforeach
+            </div>
+        </div>
+        @endforeach
     </div>
-    
+
 </div>
 
 <style>
-    .module-item{
+    .module-item {
         cursor: pointer;
     }
 </style>
