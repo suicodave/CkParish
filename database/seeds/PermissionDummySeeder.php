36fd788cfe44e14e164f79ae97c16f73365392d3
suicodave <suico.dave@gmail.com>
2019-11-21 22:16:27 +0800

    PermissionDummySeeder changed number of rows to seed

--- database/seeds/PermissionDummySeeder.php
+++ database/seeds/PermissionDummySeeder.php
@@ -12,6 +12,6 @@ class PermissionDummySeeder extends Seeder
      */
     public function run()
     {
-        factory(Permission::class, 5000)->create();
+        factory(Permission::class, 50)->create();
     }
 }
