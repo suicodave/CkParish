8994e360f81f3f7643f0f4d9ac4af0ecc7e01d42
suicodave <suico.dave@gmail.com>
2019-11-23 23:33:28 +0800

    role added global scope hidden administrator

--- app/Role.php
+++ app/Role.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Spatie\Permission\Models\Role as SpatieRole;
 
 class Role extends SpatieRole
 {
     const ADMINISTRATOR = 'administrator';
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('administrator', function (Builder $builder) {
+            $builder->where('name', '<>', self::ADMINISTRATOR);
+        });
+    }
 }
