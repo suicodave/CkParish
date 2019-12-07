ecefec6a91a0d357dd164a312bcd31dd6c62247b
suicodave <suico.dave@gmail.com>
2019-12-07 15:05:49 +0800

    base entity index implemented

---
+++ resources/views/base-entity-index.blade.php
@@ -0,0 +1,55 @@
+@extends('base-layout')
+
+
+@section('content')
+
+
+<div class="container mt-5 ">
+    <div class="col-10 offset-1">
+        <div class="card shadow-sm">
+
+
+            <div class="card-body">
+                <h3 class="card-title text-heading mb-1">
+                    {{$indexName}}s
+                </h3>
+                <div class="row ">
+
+
+                    <div class="col-12">
+
+                        @include($tableName,[$tableKey=>$users])
+
+                        <div class="row">
+                            @if ($createLink)
+                            <div class="col-3 ">
+
+                                <a href="{{route($formLink)}}" class=" btn  btn-primary ">
+                                    <span class="mr-1">New {{$indexName}}</span>
+
+                                    <span class="fa fa-plus"></span>
+                                </a>
+                            </div>
+                            @endif
+
+
+                            <div class="col">
+                                <div class=" float-right">
+                                    {{$collection->links()}}
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+@endsection
