b062fc64b10e1c440d88400c3e14da9dbb60c6b0
suicodave <suico.dave@gmail.com>
2019-12-07 15:41:53 +0800

    user table refactored via base entity table

--- resources/views/user/table.blade.php
+++ resources/views/user/table.blade.php
@@ -1,35 +1,29 @@
-<div class="table-wrapper mb-3">
-    <table class="table table-hover ">
-        <thead>
-            <tr>
-                <th class="bg-white text-heading" scope="col">#</th>
-                <th class="bg-white text-heading" scope="col">Email</th>
-                <th class="bg-white text-heading" scope="col">Name</th>
-                <th class="bg-white text-heading" scope="col">Created At</th>
-                <th class="bg-white text-heading" scope="col">Last Update</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $user)
-            <tr  >
-                <th>
-                    {{$user->id}}
-                </th>
-                <td>
-                    {{$user->email}}
-                </td>
-                <td>
-                    <a href="{{route('users.show',[
-                        'user'=>$user->id
-                        ])}}">{{ucwords($user->name)}}</a>
-                </td>
-                <td>{{$user->created_at->toDayDateTimeString()}}</td>
-                <td>{{$user->updated_at->toDayDateTimeString()}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+@extends('base-entity-table',[
+'tableHeaders'=>[
+'#',
+'Email',
+'Name',
+'Created At',
+'Last Updated'
+]
+])
 
-
-
+@section('tbody')
+@foreach ($users as $user)
+<tr>
+    <th>
+        {{$user->id}}
+    </th>
+    <td>
+        {{$user->email}}
+    </td>
+    <td>
+        <a href="{{route('users.show',[
+                'user'=>$user->id
+                ])}}">{{ucwords($user->name)}}</a>
+    </td>
+    <td>{{$user->created_at->toDayDateTimeString()}}</td>
+    <td>{{$user->updated_at->toDayDateTimeString()}}</td>
+</tr>
+@endforeach
+@endsection
