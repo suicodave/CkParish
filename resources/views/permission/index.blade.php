68a1a3b1d53e77a699ec54cdf82f544ea048537c
suicodave <suico.dave@gmail.com>
2019-12-07 15:15:17 +0800

    permission index view refactored by extending base entity index

--- resources/views/permission/index.blade.php
+++ resources/views/permission/index.blade.php
@@ -1,34 +1,7 @@
-@extends('base-layout')
-
-
-@section('content')
-<div class="container mt-5 ">
-
-    <div class="col-10 offset-1">
-        <div class="card shadow-sm">
-            <div class="card-body">
-                <h3 class="mb-3 card-title text-heading">
-                    Permissions
-                </h3>
-
-                <div class="row ">
-                    <div class="col-12">
-                        @include('permission.table',['permissions'=>$permissions])
-                    </div>
-
-                    <div class="col">
-                        <div class="float-right">
-                            {{$permissions->links()}}
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-        </div>
-
-    </div>
-</div>
-
-<div class="divider"></div>
-@endsection
+@extends('base-entity-index',[
+'createLink'=>false,
+'indexName'=>'Permission',
+'collection'=>$permissions,
+'tableName'=>'permission.table',
+'tableKey'=>'permissions'
+])
