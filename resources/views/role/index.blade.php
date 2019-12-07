dbfda700f82469cb8debbbf9206d50bb6fce9975
suicodave <suico.dave@gmail.com>
2019-12-07 15:18:23 +0800

    roles index view refactored by extending base entity index

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -1,48 +1,8 @@
-@extends('base-layout')
-
-
-@section('content')
-<div class="container mt-5 ">
-    <div class="col-10 offset-1">
-        <div class="card shadow-sm">
-            <div class="card-body">
-                <h2 class="card-title text-heading mb-1">
-                    Roles
-                </h2>
-
-                <div class="row ">
-
-                    <div class="col-12">
-                        @include('role.table',['roles'=>$roles])
-
-                        <div class="row">
-                            <div class="col-3">
-                                <a href="{{route('roles.create')}}" class="btn btn-primary">
-                                    <span class="mr-1">New Role</span>
-
-                                    <span class="fa fa-plus"></span>
-                                </a>
-                            </div>
-
-                            <div class="col">
-                                <div class="float-right">
-
-                                    {{$roles->links()}}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-
-</div>
-
-<div class="divider"></div>
-@endsection
+@extends('base-entity-index',[
+'createLink'=>true,
+'formLink'=>'roles.create',
+'tableName'=>'role.table',
+'tableKey'=>'roles',
+'collection'=>$roles,
+'indexName'=>'Role'
+])
