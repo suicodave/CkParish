ebd259d4326960d4bc08c540e75d6a1ba70085cb
suicodave <suico.dave@gmail.com>
2019-12-07 14:05:15 +0800

    added customer migration file

---
+++ database/migrations/2019_12_07_010533_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->date('birthdate');
+            $table->integer('created_by');
+            $table->timestamps();
+
+            $table->index(['first_name', 'middle_name', 'last_name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
