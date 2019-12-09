7767a28de8f93450f573eefa321b7f04d04bfebf
suicodave <suico.dave@gmail.com>
2019-12-09 09:26:30 +0800

    role table refactored by extending base-entity-table

--- resources/views/role/table.blade.php
+++ resources/views/role/table.blade.php
@@ -1,28 +1,25 @@
-<div class="table-wrapper mb-3">
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th class="bg-white text-heading" scope="col">#</th>
-                <th class="bg-white text-heading" scope="col">Name</th>
-                <th class="bg-white text-heading" scope="col">Created At</th>
-                <th class="bg-white text-heading" scope="col">Last Update</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($roles as $role)
-            <tr>
-                <th>
-                    {{$role->id}}
-                </th>
-                <td>
-                    <a href="{{route('roles.show',[
+@extends('base-entity-table',[
+'tableHeaders'=>[
+'#',
+'Name',
+'Created At',
+'Last Updated'
+]
+])
+
+@section('tbody')
+@foreach ($roles as $role)
+<tr>
+    <th>
+        {{$role->id}}
+    </th>
+    <td>
+        <a href="{{route('roles.show',[
                     'role'=>$role->id
                     ])}}">{{ucwords($role->name)}}</a>
-                </td>
-                <td>{{$role->created_at->toDayDateTimeString()}}</td>
-                <td>{{$role->updated_at->toDayDateTimeString()}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+    </td>
+    <td>{{$role->created_at->toDayDateTimeString()}}</td>
+    <td>{{$role->updated_at->toDayDateTimeString()}}</td>
+</tr>
+@endforeach
+@endsection
