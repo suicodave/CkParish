46bab1944486b784b2030ee4cff2f1eba3dfff23
suicodave <suico.dave@gmail.com>
2019-12-09 09:32:58 +0800

    permission table refactored via extending base entity table

--- resources/views/permission/table.blade.php
+++ resources/views/permission/table.blade.php
@@ -1,25 +1,19 @@
-<div class="table-wrapper">
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th class="bg-white text-heading" scope="col">#</th>
-        <th class="bg-white text-heading" scope="col">Name</th>
-        <th class="bg-white text-heading" scope="col">Created At</th>
-        <th class="bg-white text-heading" scope="col">Last Update</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($permissions as $permission)
-      <tr>
-        <th>{{$permission->id}}</th>
-        <td>{{ucwords($permission->name)}}</td>
-        <td>{{$permission->created_at->toDayDateTimeString()}}</td>
-        <td>{{$permission->updated_at->toDayDateTimeString()}}</td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
-</div>
+@extends('base-entity-table',[
+'tableHeaders'=>[
+'#',
+'Name',
+'Created At',
+]
+])
 
+@section('tbody')
 
+@foreach ($permissions as $permission)
+<tr>
+  <th>{{$permission->id}}</th>
+  <td>{{ucwords($permission->name)}}</td>
+  <td>{{$permission->created_at->toDayDateTimeString()}}</td>
+</tr>
+@endforeach
 
+@endsection
