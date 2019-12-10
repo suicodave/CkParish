81f1f4bd011a9940da11d624e9bf567569444384
suicodave <suico.dave@gmail.com>
2019-12-10 18:17:44 +0800

    added laravel dompdf

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
 
         Spatie\Permission\PermissionServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -228,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
