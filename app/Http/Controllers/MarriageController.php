bb2d233a4eee78cd150d66b25fffea129990f77c
suicodave <suico.dave@gmail.com>
2020-01-06 13:11:58 +0800

    initial files for marriage entity

---
+++ app/Http/Controllers/MarriageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Marriage;
+use Illuminate\Http\Request;
+
+class MarriageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Marriage  $marriage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Marriage $marriage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Marriage  $marriage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Marriage $marriage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Marriage  $marriage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Marriage $marriage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Marriage  $marriage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Marriage $marriage)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_01_05_204210_create_marriages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarriagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marriages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('customer_id');
+            $table->string('priest_name');
+            $table->date('wedding_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marriages');
+    }
+}
