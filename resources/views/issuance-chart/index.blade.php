eb73d92c07e390461fde24ba42431dc07f3f64a4
suicodave <suico.dave@gmail.com>
2020-03-10 23:27:49 +0800

    view chart check if no data

--- resources/views/issuance-chart/index.blade.php
+++ resources/views/issuance-chart/index.blade.php
@@ -11,10 +11,11 @@
         <div class="col-12">
             <h1 class="text-center">Certificate Issuances</h1>
 
+            @isset($chart)
             <div>
                 {!! $chart->container() !!}
             </div>
-
+            @endisset
 
         </div>
 
@@ -30,7 +31,8 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
 
+@isset($chart)
 {!! $chart->script() !!}
-
+@endisset
 
 @endsection
